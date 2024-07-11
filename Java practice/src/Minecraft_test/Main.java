6240cacee2bca089cd144f66316652ebe2369424
Glitch Master (aka Justin) <71327490+Glitch-m@users.noreply.github.com>
2024-07-11 14:56:46 -0400

    Add files via upload

---
+++ Java practice/src/Minecraft_test/Main.java
@@ -0,0 +1,23 @@
+package Minecraft_test;
+
+public class Main {
+    public static void main(String[] args) {
+        /* Task
+        *
+        *  Create a custom class and connect it to the main class
+        *  Make sure that when you Run the code you can make a Java collection, interfaces & Polymorphism classes
+        *  Create a custom statement within these classes and complete the code of the other classes for the main class to work.
+        *
+        *  primary objective is to make a Java collection with a proper name. The name we will use for this class will be called "The_minecraft_Game"
+        *  The Second class that supports the first class will be called "The_Minecraft_Config"
+        *
+        *  The "The_minecraft_Config" Class will be used to set up print out statements for the main class. By the time we finish the Code
+        *  The main class will have a code that says "The_Minecraft_Game game = new The_minecraft_Game"
+        *  Hopefully This will succeed.
+        *
+        * */
+
+        // ----------------------------------------------------------------
+        // Command line arguments in this area
+    }
+}
---
+++ Java practice/src/Minecraft_test/Theminecraftgame.java
@@ -0,0 +1,4 @@
+package Minecraft_test;
+
+public class Theminecraftgame {
+}
